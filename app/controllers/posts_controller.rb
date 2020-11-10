ae97aec4de9dd3534a3b814df2b14c6e75fb3061
seiichi621 <sei-0941-seiichi5@softbank.ne.jp>
2020-11-10 11:59:43 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,17 @@
 class PostsController < ApplicationController
+
   def index
-    @posts = Post.all
+    @posts = Post.all.order(id: "DESC")
   end
-
-  def new
-  end
-
+ 
+   # 削除
+   # def new
+   # end
+ 
   def create
     Post.create(content: params[:content])
-end
+    redirect_to action: :index
+  end
+ 
+ end
+ 
